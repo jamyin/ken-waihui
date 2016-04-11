07d9cafd0ea6a4d2a1efad258388d7f401acd246
mark <mark_chu@ssic.cn>
2016-04-11 13:22:27 +0800

    11

--- waihui-admin-web/src/main/webapp/plugins/umeditor1.2.2/umeditor.config.js
+++ waihui-admin-web/src/main/webapp/plugins/umeditor1.2.2/umeditor.config.js
@@ -138,7 +138,7 @@
         //图片上传配置区
 //        ,imageUrl:URL+"jsp/imageUp.jsp"             //图片上传提交地址
         ,imageUrl:"/admin/load/configum.do"             //图片上传提交地址        
-        ,imagePath:"http://www.jujusports.cn/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
+        ,imagePath:"http://www.jujucatering.com/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         ,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
 
